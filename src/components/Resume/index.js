36063391ae67d096c8162804c6427f91bc81a30f
Nimit Patel <np23295@gmail.com>
2022-09-05 15:18:49 -0400

    Implemented resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import resume from '../../assets/documents/resume.pdf'
 
 
 const Resume = () => {
-    return (<p>Resume</p>);
+    return (<embed src={`${resume}#toolbar=1&navpanes=0&scrollbar=0`} width="100%" height="800px" />);
 }
 
 export default Resume;
